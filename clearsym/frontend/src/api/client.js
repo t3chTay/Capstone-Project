9610689a1d82ce0455c107d08af67a0a235fe6f6
t3chTay <tayjohnson101@icloud.com>
2026-02-26 18:14:24 -0500

    fix to client.js for render

--- clearsym/frontend/src/api/client.js
+++ clearsym/frontend/src/api/client.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://127.0.0.1:5001/api";
+const baseURL = import.meta.env.VITE_API_URL || "https://clearsym-web.onrender.com/api";
 
 export const API = axios.create({ baseURL });

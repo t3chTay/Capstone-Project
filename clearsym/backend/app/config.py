23d16bcf8c887bbc321527b9458f4548cee1d1c4
t3chTay <tayjohnson101@icloud.com>
2026-02-26 16:17:27 -0500

    requirements for render deployment

--- clearsym/backend/app/config.py
+++ clearsym/backend/app/config.py
@@ -6,10 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-    DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = (
-        os.getenv("DATABASE_URL")
-        or "sqlite:///" + os.path.join(basedir, "..", "instance", "clearsym.db")
-    )
+    DEBUG = os.getenv("FLASK_DEBUG", "0") == "1"
+
+    db_url = os.getenv("DATABASE_URL")
+
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///" + os.path.join(basedir, "..", "instance", "clearsym.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
